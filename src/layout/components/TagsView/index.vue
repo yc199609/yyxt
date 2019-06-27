b44df925a55d0aeb1a5bb0f7a92f4f02564c36fd
yc <3095220780@qq.com>
2019-06-27 15:15:39 +0800

    页签

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -1,194 +1,215 @@
 <template>
-  <div id="tags-view-container" class="tags-view-container">
-    <el-row>
-      <scroll-pane ref="scrollPane" class="tags-view-wrapper">
-        <router-link v-for="tag in visitedViews" ref="tag" :key="tag.path" :class="isActive(tag)?'active':''" :to="{ path: tag.path, query: tag.query, fullPath: tag.fullPath }"
-          tag="span" class="tags-view-item"  @contextmenu.prevent.native="openMenu(tag,$event)">
-          <span>{{tag.title}}</span>
-          <span v-if="!tag.meta.affix" class="el-icon-close" @click.prevent.stop="closeSelectedTag(tag)" />
+
+  <el-card :class="classObject" class="scrollbarContainer">
+
+    <div class="routers">
+      <vuescroll :ops="ops">
+        <router-link
+          v-for="tag in visitedViews"
+          ref="tag"
+          :key="tag.path"
+          :to="{ path: tag.path, query: tag.query, fullPath: tag.fullPath }"
+          tag="span"
+          class="tags-view-item"
+          @click.middle.native="closeSelectedTag(tag)"
+
+        >
+          <el-tag
+            :type="isActive(tag)?'success':'info'"
+            :closable="!tag.meta.affix"
+            class="tag"
+            style="margin-right:2px;"
+            @close="closeSelectedTag(tag)"
+          >
+            {{ tag.title }}
+          </el-tag>
         </router-link>
-      </scroll-pane>
-    </el-row>
-  </div>
+      </vuescroll>
+    </div>
+
+    <div class="contextmenu">
+      <el-tooltip
+        effect="dark"
+        class="tagButton"
+        content="关闭其他"
+        placement="top"
+      >
+        <el-button
+          class="optionbtns"
+          icon="el-icon-close"
+          circle
+          @click="closeOthersTags()"
+        />
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        class="tagButton"
+        content="关闭所有"
+        placement="top"
+      >
+        <el-button
+          class="optionbtns"
+          icon="el-icon-delete"
+          circle
+          @click="closeAllTags(selectedTag)"
+        />
+      </el-tooltip>
+    </div>
+  </el-card>
 </template>
 
 <script>
-import ScrollPane from "./ScrollPane";
-import path from "path";
-
+import vuescroll from 'vuescroll'
 export default {
-  components: { ScrollPane },
+  components: {
+    vuescroll
+  },
   data() {
     return {
       top: 0,
       left: 0,
       selectedTag: {},
-      affixTags: []
-    };
+      affixTags: [],
+      ops: {
+        vuescroll: {},
+        scrollPanel: {},
+        rail: {
+          opacity: 0.5,
+          background: '#cecece'
+        },
+        bar: {
+          background: '#cecece'
+        }
+      }
+    }
   },
   computed: {
     visitedViews() {
-      return this.$store.state.tagsView.visitedViews;
+      return this.$store.state.tagsView.visitedViews
+    },
+    routes() {
+      return this.$store.state.permission.routes
+    },
+    sidebar: function() {
+      return this.$store.state.app.sidebar
+    },
+    classObject: function() {
+      return {
+        sidebarOpen: this.sidebar.opened
+      }
     }
   },
   watch: {
     $route() {
-      this.addTags();
-    },
-    visible(value) {
-      if (value) {
-        document.body.addEventListener("click", this.closeMenu);
-      } else {
-        document.body.removeEventListener("click", this.closeMenu);
-      }
+      this.addTags()
+      this.moveToCurrentTag()
     }
   },
   mounted() {
-    this.initTags();
-    this.addTags();
+    this.addTags()
   },
   methods: {
     isActive(route) {
-      return route.path === this.$route.path;
-    },
-    initTags() {
-      const affixTags = this.affixTags;
-      for (const tag of affixTags) {
-        if (tag.name) {
-          this.$store.dispatch("tagsView/addVisitedView", tag);
-        }
-      }
+      return route.path === this.$route.path
     },
     addTags() {
-      const { name } = this.$route;
+      const { name } = this.$route
       if (name) {
-        this.$store.dispatch("tagsView/addView", this.$route);
+        this.$store.dispatch('tagsView/addView', this.$route)
       }
-      return false;
+      return false
+    },
+    moveToCurrentTag() {
+      const tags = this.$refs.tag
+      this.$nextTick(() => {
+        for (const tag of tags) {
+          if (tag.to.path === this.$route.path) {
+            if (tag.to.fullPath !== this.$route.fullPath) {
+              this.$store.dispatch('tagsView/updateVisitedView', this.$route)
+            }
+            break
+          }
+        }
+      })
     },
     closeSelectedTag(view) {
-      this.$store
-        .dispatch("tagsView/delView", view)
-        .then(({ visitedViews }) => {
-          if (this.isActive(view)) {
-            this.toLastView(visitedViews, view)
-          }
-        })
+      this.$store.dispatch('tagsView/delView', view).then(({ visitedViews }) => {
+        if (this.isActive(view)) {
+          this.toLastView(visitedViews, view)
+        }
+      })
+    },
+    closeOthersTags() {
+      this.$router.push(this.selectedTag)
+      this.$store.dispatch('tagsView/delOthersViews', this.selectedTag).then(() => {
+        this.moveToCurrentTag()
+      })
+      this.addTags()
+    },
+    closeAllTags(view) {
+      this.$store.dispatch('tagsView/delAllViews').then(({ visitedViews }) => {
+        if (this.affixTags.some(tag => tag.path === view.path)) {
+          return
+        }
+        this.toLastView(visitedViews, view)
+      })
     },
     toLastView(visitedViews, view) {
-      const latestView = visitedViews.slice(-1)[0];
+      const latestView = visitedViews.slice(-1)[0]
       if (latestView) {
-        this.$router.push(latestView);
+        this.$router.push(latestView)
       } else {
-        if (view.name === "Dashboard") {
-          this.$router.replace({ path: "/redirect" + view.fullPath });
+        if (view.name === 'Dashboard') {
+          this.$router.replace({ path: '/redirect' + view.fullPath })
         } else {
-          this.$router.push("/");
+          this.$router.push('/')
         }
       }
-    },
-    openMenu(tag, e) {
-      const menuMinWidth = 105;
-      const offsetLeft = this.$el.getBoundingClientRect().left; // container margin left
-      const offsetWidth = this.$el.offsetWidth; // container width
-      const maxLeft = offsetWidth - menuMinWidth; // left boundary
-      const left = e.clientX - offsetLeft + 15; // 15: margin right
-      if (left > maxLeft) {
-        this.left = maxLeft;
-      } else {
-        this.left = left;
-      }
-      this.top = e.clientY;
-      this.visible = true;
-      this.selectedTag = tag;
-    },
-    closeMenu() {
-      this.visible = false;
     }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
-.tags-view-container {
-  top: 50px;
-  z-index: 1000;
-  height: 34px;
-  width: 100%;
-  background: #fff;
-  border-bottom: 1px solid #d8dce5;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
+.tag{
+  cursor: pointer;
+}
+.sidebarOpen{
+  min-width: auto !important;
+  width: 83vw !important;
+  @media screen and (min-width: 1230px){
+    width:calc(100vw - 182px) !important;
+  }
+}
+.scrollbarContainer{
   position: fixed;
-  .tags-view-wrapper {
-    .tags-view-item {
-      display: inline-block;
-      position: relative;
-      cursor: pointer;
-      height: 26px;
-      line-height: 26px;
-      border: 1px solid #d8dce5;
-      color: #495060;
-      background: #fff;
-      padding: 0 8px;
-      font-size: 12px;
-      margin-left: 5px;
-      margin-top: 4px;
-      &:first-of-type {
-        margin-left: 15px;
-      }
-      &:last-of-type {
-        margin-right: 15px;
-      }
-      &.active {
-        background-color: #42b983;
-        color: #fff;
-        border-color: #42b983;
-        &::before {
-          content: "";
-          background: #fff;
-          display: inline-block;
-          width: 8px;
-          height: 8px;
-          border-radius: 50%;
-          position: relative;
-          margin-right: 2px;
-        }
-      }
+  z-index: 100;
+  top: 51px;
+  width: 100%;
+  height: 32px;
+  .routers{
+    white-space: nowrap;
+    margin-right:120px;
+    height: 32px;
+    // background-color:orange;
+  }
+  .contextmenu{
+    // background-color: red;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 120px;
+    height: 32px;
+    line-height: 30px;
+    .optionbtns{
+      padding: 5px;
     }
   }
-  .contextmenu {
-    height: 34px;
-    line-height: 34px;
-    float: right;
-    .tagButton {
-      padding: 6px;
-    }
-  }
+}
+</style>
+<style scoped>
+.scrollbarContainer >>> .el-card__body{
+  padding: 0;
 }
 </style>
 
-<style lang="scss">
-//reset element css of el-icon-close
-.tags-view-wrapper {
-  .tags-view-item {
-    .el-icon-close {
-      width: 16px;
-      height: 16px;
-      vertical-align: 2px;
-      border-radius: 50%;
-      text-align: center;
-      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-      transform-origin: 100% 50%;
-      &:before {
-        transform: scale(0.6);
-        display: inline-block;
-        vertical-align: -3px;
-      }
-      &:hover {
-        background-color: #b4bccc;
-        color: #fff;
-      }
-    }
-  }
-}
-</style>
