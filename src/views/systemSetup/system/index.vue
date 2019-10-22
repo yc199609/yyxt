5d4b67104303e982a792024fee94e2f37ff69438
章丹 <zhangdan@lqwydeiMac.local>
2019-10-22 18:01:57 +0800

    初始化缓存中的数据库表格Id值

--- src/views/systemSetup/system/index.vue
+++ src/views/systemSetup/system/index.vue
@@ -3,6 +3,8 @@
 
     <Search :show-btn="buttonPermissions('93000201')" @search='init' @changeKeyword='changeKeyword'/>
 
+    <el-button style="position: absolute; top: 40px; right: 5vw" type="primary" icon="el-icon-refresh" plain @click="initTableValue">初始化缓存中的数据库表格Id值</el-button>
+
     <el-card>
        <el-table :data="tableData" border class="table">
         <el-table-column align="center" prop="code" label="代码">
@@ -46,7 +48,7 @@
 
 <script>
 import Search from "@/components/Search";
-import { SystemConfigList, SystemConfigUpdate } from "@api/systemSetup/system";
+import { SystemConfigList, SystemConfigUpdate, InitTableID } from "@api/systemSetup/system";
 import { buttonPermissions } from '@/mixin'
 
 export default {
@@ -73,6 +75,15 @@ export default {
     Search
   },
   methods: {
+    initTableValue() {
+      InitTableID().then(res => {
+         this.$message({
+          type: "success",
+          message: "初始化成功",
+          duration: 500
+        });
+      })
+    },
     changeKeyword(val){
       this.keyword = val
     },
