5ab3350bc94ba91e0b46f5cd38a694808350c196
yc <3095220780@qq.com>
2019-12-18 11:55:56 +0800

    模板导入 视图校正

---
+++ src/views/instructions/templateImport/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="contain">
+    <Search :show-btn="true" @search="init" @changeKeyword="changeKeyword">
+      <el-button type="warning" plain size="small" icon="el-icon-plus" @click="insert">新增</el-button>
+    </Search>
+  
+    <el-card>
+      <el-table :data="tableData">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column label="操作" >
+          <template slot-scope="scope">
+            <el-button @click="edit(scope.row.id)">修改</el-button>
+            <el-button @click="del(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+
+    <el-pagination
+      :current-page="pageIndex"
+      :page-sizes="[5 ,20, 30, 40, 50]"
+      :page-size="pageSize"
+      :total="totalCount"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      />
+    <Detail v-if="detailVisible" ref="detail" @reload='hidden'/>
+  </div>
+</template>
+<script>
+import Detail from './details'
+import { GetList, del } from '@/api/templateImport'
+import Search from '@/components/Search'
+import { pagging, keyword } from '@/mixin'
+export default {
+  data(){
+    return {
+      tableData:[],
+      detailVisible:false
+    }
+  },
+  components: {
+    Search,
+    Detail
+  },
+  mixins: [pagging, keyword],
+  mounted(){
+    this.init()
+  },
+  methods:{
+
+    hidden(){
+      this.detailVisible = false
+      this.init()
+    },
+    init(){
+      GetList({
+        keyword: this.keyword,
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      }).then(res=>{
+        this.$set(this,'tableData',res.data.items)
+      })
+    },
+    edit(id){
+      this.detailVisible = true
+      this.$nextTick(()=>{
+        this.$refs.detail.init(id)
+      })
+    },
+    del(id){
+      del(id)
+        .then(res=>{
+          this.$message({
+            type:"success",
+            message:'删除成功',
+            duration:500,
+            onClose:()=>{
+              this.init()
+            }
+          })
+        })
+    },
+    insert(){
+      this.detailVisible = true
+      this.$nextTick(()=>{
+        this.$refs.detail.init()
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .contain{
+    padding: 1vw 3vw 0;
+  }
+</style>
