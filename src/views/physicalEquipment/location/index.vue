e2c455257199dcadd9203ed390323ebe4f8b6d05
章丹 <zhangdan@lqwydeiMac.local>
2020-01-08 18:22:09 +0800

    123

--- src/views/physicalEquipment/location/index.vue
+++ src/views/physicalEquipment/location/index.vue
@@ -13,24 +13,25 @@
         <el-table-column align="center" prop="description" label="部位描述"/>
         <el-table-column align="center" prop="deviceTypeId" label="设备类型Id"/>
 
-        <el-table-column align="center" label="操作">
+        <!-- <el-table-column align="center" label="操作">
           <template slot-scope="scope">
             <el-row>
-              <!-- <el-col :offset="3" :span="8" class="ycbutton">
+              <el-col :offset="3" :span="8" class="ycbutton">
                 <el-tooltip effect="dark" content="编辑" placement="top">
                   <el-button type="warning" icon="el-icon-edit-outline" @click="edit(scope.row)"/>
                 </el-tooltip>
-              </el-col> -->
+              </el-col>
 
-              <!-- <el-col :span="8" :offset="2" class="ycbutton">
+              <el-col :span="8" :offset="2" class="ycbutton">
                 <el-tooltip effect="dark" content="删除" placement="top">
                   <el-button type="danger" icon="el-icon-delete" @click="del(scope.row.id)"/>
                 </el-tooltip>
-              </el-col> -->
+              </el-col>
 
             </el-row>
           </template>
-        </el-table-column>
+        </el-table-column> -->
+        
       </el-table>
     </el-card>
 
