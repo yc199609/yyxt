8416565992a41bd87510fb31cd136ba0aebbd417
章丹 <zhangdan@lqwydeiMac.local>
2020-01-09 09:44:30 +0800

    注释指标字段

--- src/router/index.js
+++ src/router/index.js
@@ -162,7 +162,8 @@ export const asyncRouterMap = [
         path: 'field',
         component: () => import('@/views/instructions/field'),
         name: 'field',
-        meta: { title: '指标字段', icon: 'instructions-field', code: '9200' }
+        meta: { title: '指标字段', icon: 'instructions-field', code: '9200' },
+        hidden: true // 注释指标字段
       },
       {
         path: 'templateModify',
